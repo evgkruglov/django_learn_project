b61b875a45b69f965122f303bb0edbbec5ed24ea
evgkruglov <evgkukharenko@gmail.com>
2023-12-11 23:47:20 +0300

    first part done

---
+++ sitewomen/women/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("Страница приложения women.")
+
+
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p >id:{cat_id}</p>")
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p >slug:{ cat_slug }</p>")
+
+
+def archive(request, year):
+    if year > 2023:
+        return redirect('home', permanent=True)
+    return HttpResponse(f"<h1>Архив по годам</h1><p >{year}</p>")
+
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
